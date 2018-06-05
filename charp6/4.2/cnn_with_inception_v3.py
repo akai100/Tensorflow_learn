0bb7604e3d1c280dac58f1e547afcd7d7ac016ed
akai100 <1107971906@qq.com>
2018-06-05 08:40:05 +0800

    push

--- charp6/4.2/cnn_with_inception_v3.py
+++ charp6/4.2/cnn_with_inception_v3.py
@@ -3,7 +3,7 @@
 使用TenforFlow-Slim工具来实现一个卷积层
 """
 import tensorflow as tf
-import tensorflow.contrib.slim
+import tensorflow.contrib.slim as slim
 
 # 直接使用TensorFlow原始API实现卷积层。
 with tf.variable_scope("scope_name"):
@@ -29,4 +29,43 @@ with slim.arg_scope([slim.conv2d, slim.max_pool2d, slim.avg_pool2d],
     # 此处省略了Inception-v3模型中其他的网络结构而直接实现后面红色方框中的
     # Inception结构。假设输入图片经过之前的神经网络前向传播的结果保存在变量net
     # 中
-    net = 上一层的输出节点矩阵
+    # net = 上一层的输出节点矩阵
+    # 为一个Inception模块声明一个统一的变量命名空间
+    with tf.variable_scope('Mixed_7c'):
+
+        #  给Inception模块中每一条路经声明一个命名空间
+        with tf.variable_scope('Branch_0'):
+            # 实现一个过滤器边长为１，深度为３２０的卷积层
+            branch_0 = slim.conv2d(net, 320, [1, 1], scope='Conv2d_0a_1x1')
+
+        # Inception模块中第二条路径。这条计算路径上的结构本身也是一个Inception结构
+        with tf.variable_scope('Branch_1'):
+            branch_1 = slim.conv2d(net, 384, [1, 1], scope='Conv2d_0a_1x1')
+            # tf.concat寒素可以将多个矩阵拼接起来。tf.concat函数的第一个参数指定
+            # 了拼接的维度，这里给出的“3”代表了矩阵是在深度这个维度上进行拼接
+            branch_1 = tf.concat(3, [
+                slim.conv2d(branch_1, 384, [1, 3], scope='Conv2d_0b_1x3'),
+                slim.conv2d(branch_1, 384, [3, 1], scope='Conv2d_0c_3x1')
+            ])
+
+        # Inception模块中第三条路径。此计算路径也是一个Inception结构
+        with tf.variable_scope('Branch_2'):
+            branch_2 = slim.conv2d(
+                net, 448, [1, 1], scope='Conv2d_0a_1x1')
+            branch_2 = slim.conv2d(
+                branch_2, 384, [3, 3], scope='Conv2d_ob_3x3')
+            branch_2 = tf.concat(3, [
+                slim.conv2d(branch_2, 384, [1, 3], scope='Conv2d_0c_1x3'),
+                slim.conv2d(branch_2, 384, [3, 1], scope='Conv2d_0d_3x1')])
+
+        # Inception模块中第四条路径
+        with tf.variable_scope("Branch_3"):
+            branch_3 = slim.avg_pool2d(
+                net, [3, 3], scope='AvgPool_0a_3x3')
+            branch_3 = slim.conv2d(
+                branch_3, 192, [1, 1], scope='Cov2d_0b_1x1')
+
+        # 当前Inception模块的最后输出是由上面四个计算结果拼接得到的。
+        net = tf.concat(3, [branch_0, branch_1, branch_2, branch_3])
+
+
